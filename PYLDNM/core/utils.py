bee477ea4846560651e7d5b2f9277f2c100fe4c1
OmarAhmedHDS <146376387+OmarAhmedHDS@users.noreply.github.com>
2025-06-12 18:01:46 +0100

    .

--- PYLDNM/core/utils.py
+++ PYLDNM/core/utils.py
@@ -1,1 +1,17 @@
+import numpy as np
+
+def lag_matrix(x, max_lag):
+    """
+    Construct a lag matrix for a 1D array x with lags from 0 to max_lag.
+    Each row t contains [x[t], x[t-1], ..., x[t-max_lag]]
+    """
+    x = np.asarray(x)
+    n = len(x)
+    L = max_lag + 1
+    mat = np.full((n, L), np.nan)
+
+    for lag in range(L):
+        mat[lag:, lag] = x[:n - lag]
+
+    return mat
 
